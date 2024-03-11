32810009e2c079fa9e29b00539e2fe352f4b5b35
Alexandra <s7125@mail.ru>
2024-03-11 09:13:43 +0500

    Initial Commit

---
+++ TinkofCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TinkofCalculator
+//
+//  Created by Sandra on 10.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("Start-booom")
+    }
+
+
+}
+
