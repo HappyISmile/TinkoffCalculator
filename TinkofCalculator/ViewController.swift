ab95ea3bc043d85fe20d69130d8e126918052bd6
Alexandra <s7125@mail.ru>
2024-03-19 11:40:49 +0500

    rename

--- TinkofCalculator/ViewController.swift
+++ TinkofCalculator/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBAction func buttonPressed(_ sender: UIButton) {
-        guard let buttonText = sender.currentTitle else{ return }
+        guard let buttonText = sender.currentTitle else { return }
         
         print(buttonText)
     }
